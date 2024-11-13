6bc341c0d35ba38cd2fd61df393c60d7217de072
Jaydeep Pampaniya <105541712+JaydeepPampaniya@users.noreply.github.com>
2024-11-13 10:16:46 +0530

    Delete react/src/Components  directory

--- react/src/Components /Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({type,buttonName,className,onClick}) => {
-  return <button type={type} className={className} onClick={onClick}>{buttonName}</button>;
-};
-
-export default Button;
