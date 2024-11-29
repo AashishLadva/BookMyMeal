b7f8d8dce8bce8fda220b121d96f44fc1acee3da
Jaydeep Pampaniya <105541712+JaydeepPampaniya@users.noreply.github.com>
2024-11-29 10:22:42 +0530

    Delete Frontend directory

--- Frontend/src/Components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({type,buttonName,className,onClick,databstoggle,databstarget}) => {
-  return <button type={type} className={className} onClick={onClick} data-bs-toggle={databstoggle} data-bs-target={databstarget}>{buttonName}</button>;
-};
-
-export default Button;
